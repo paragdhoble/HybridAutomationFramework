3a5004bcca8f52d01ad09d41027123063d03162a
paragdhoble <paragd001@gmail.com>
2020-04-17 16:57:34 +0530

    Formating

--- src/main/java/com/github/paragdhoble/util/TestUtil.java
+++ src/main/java/com/github/paragdhoble/util/TestUtil.java
@@ -3,17 +3,19 @@ package com.github.paragdhoble.util;
 import com.github.paragdhoble.base.TestBase;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
+
 import java.io.File;
 import java.io.IOException;
+
 import org.apache.commons.io.*;
 
 public class TestUtil extends TestBase {
 
-    public static long PAGE_LOAD_TIMEOUT = 20 ;
-    public static long IMPLICIT_WAIT = 10 ;
+    public static long PAGE_LOAD_TIMEOUT = 20;
+    public static long IMPLICIT_WAIT = 10;
 
 
-    public  static  void takeScreenshotAtEndOfTest () throws IOException {
+    public static void takeScreenshotAtEndOfTest() throws IOException {
         File scrFile = ((TakesScreenshot) driver).getScreenshotAs(OutputType.FILE);
         String currentDir = System.getProperty("user.dir");
         FileUtils.copyFile(scrFile, new File(currentDir + "/screenshots/" + System.currentTimeMillis() + ".png"));
