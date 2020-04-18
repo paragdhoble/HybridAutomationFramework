05b485d5561880d3d8a6924bc968992d9740c76f
paragdhoble <paragd001@gmail.com>
2020-04-18 12:50:34 +0530

    Added functionality to read data from Exel file and added common function in the util

--- src/main/java/com/github/paragdhoble/pages/ContactPage.java
+++ src/main/java/com/github/paragdhoble/pages/ContactPage.java
@@ -1,6 +1,45 @@
 package com.github.paragdhoble.pages;
 
 import com.github.paragdhoble.base.TestBase;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 public class ContactPage extends TestBase {
+
+    @FindBy(xpath = "(//button[contains(@class,'ui linkedin')])[3]")
+    WebElement newContact ;
+
+    @FindBy(xpath = "(//button[contains(@class,'ui linkedin button')])[2]")
+    WebElement saveContact ;
+
+
+    @FindBy (name = "first_name")
+    WebElement firstName ;
+
+    @FindBy (name = "last_name")
+    WebElement lastName ;
+
+    @FindBy (name = "company")
+    WebElement company ;
+
+    public ContactPage (){
+        PageFactory.initElements(driver , this);
+
+    }
+
+    //Function
+
+    public void createNewContact (String fName , String lName , String companyName) {
+        newContact.click();
+        firstName.sendKeys(fName);
+        lastName.sendKeys(lName);
+        company.sendKeys(companyName);
+        saveContact.click();
+
+    }
+
+
 }
+
+
